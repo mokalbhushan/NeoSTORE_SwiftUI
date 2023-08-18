266780dc7c9844e9a2d86ecafc7544754fc3aea9
Bhushan Mokal <bhushanmokal02@gmail.com>
2023-08-18 16:21:41 +0530

    Initial Commit

---
+++ NeoSTORE/NeoSTOREApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoSTOREApp.swift
+//  NeoSTORE
+//
+//  Created by Bhushan Mokal on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NeoSTOREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
